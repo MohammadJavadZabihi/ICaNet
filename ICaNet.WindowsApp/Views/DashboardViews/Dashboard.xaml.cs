838ad4191739a70b939b379b93013157957747ba
Toxix98 <128359759+Toxix98@users.noreply.github.com>
2025-02-28 16:33:34 +0330

    start to desgin and coding the profile user

---
+++ ICaNet.WindowsApp/Views/DashboardViews/Dashboard.xaml.cs
@@ -0,0 +1,97 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ICaNet.WindowsApp.Views.DashboardViews
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Page
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            LoadChart();
+        }
+
+        private void LoadChart()
+        {
+            positiveProfit.AxisX.Add(new Axis
+            {
+                Labels = new List<string>
+                {
+                    "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",
+                    "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"
+                },
+                ShowLabels = true,
+                Foreground = Brushes.White,
+            });
+
+            positiveProfit.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<double> { 1000000, 1500000, 1200000, 2000000, 2500000, 2300000, 1900000, 2100000, 2400000, 2200000, 2700000, 2900000 },
+                    Title = null,
+                    Fill = new SolidColorBrush(Color.FromRgb(70, 130, 180))
+                }
+            };
+
+            negativeProfit.AxisX.Add(new Axis
+            {
+                Labels = new List<string>
+                {
+                    "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",
+                    "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"
+                },
+                ShowLabels = true,
+                Foreground = Brushes.White,
+            });
+
+            negativeProfit.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<double> { 1000000, 1500000, 1200000, 2000000, 2500000, 2300000, 1900000, 2100000, 2400000, 2200000, 2700000, 2900000 },
+                    Title = null,
+                    Fill = new SolidColorBrush(Color.FromRgb(70, 130, 180))
+                }
+            };
+
+            totalProfit.AxisX.Add(new Axis
+            {
+                Labels = new List<string>
+                {
+                    "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",
+                    "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"
+                },
+                ShowLabels = true,
+                Foreground = Brushes.White,
+            });
+
+            totalProfit.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<double> { 1000000, 1500000, 1200000, 2000000, 2500000, 2300000, 1900000, 2100000, 2400000, 2200000, 2700000, 2900000 },
+                    Title = null,
+                    Fill = new SolidColorBrush(Color.FromRgb(70, 130, 180))
+                }
+            };
+        }
+    }
+}
--- ICaNet.WindowsApp/Views/MainWindow.xaml.cs
+++ ICaNet.WindowsApp/Views/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ICaNet.WindowsApp.Views.DashboardViews;
 
 namespace ICaNet.WindowsApp.Views
 {
@@ -25,6 +26,7 @@ namespace ICaNet.WindowsApp.Views
     {
         private string? apiUrl = ConfigurationManager.AppSettings["ApiURL"];
         private string? apiVersion = ConfigurationManager.AppSettings["ApiVersion"];
+        private Button _selectedButton;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,9 +34,42 @@ namespace ICaNet.WindowsApp.Views
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var apiService = new CallApi();
 
-            var response = await apiService.SendPostRequest<UserGetDashboardResponse>($"{apiUrl}/api/v{apiVersion}/user/GetDashboard", jwt: TokenManager.Token);
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (this.WindowState != WindowState.Maximized)
+                    this.WindowState = WindowState.Maximized;
+                else
+                    this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedButton != null)
+            {
+                _selectedButton.Background = new SolidColorBrush(Colors.Transparent);
+                _selectedButton.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f1f1f1"));
+            }
+
+            _selectedButton = sender as Button;
+
+            if (_selectedButton != null)
+            {
+                _selectedButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#737373"));
+                _selectedButton.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f1f1f1"));
+            }
+
+            Dashboard dashboard = new Dashboard();
+            fContainer.Navigate(dashboard);
         }
     }
 }
